fd2d7346c2aa5a19ada9ac2df4fbcb1f540b4db3
Chako <ryland0000@gmail.com>
2023-11-01 11:03:51 -0700

    sample test 3

--- sample_test.py
+++ sample_test.py
@@ -5,17 +5,33 @@ def sum(x, y):
     return result
 
 class SampleTest(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum(4, 6), 0)
+        self.assertEqual(sum(-4, -6), -10)
+        self.assertEqual(sum(4.5, 6.2), 10.7)
+        self.assertEqual(sum(-4.5, -6.2), -10.7)
+
+   """
+    def setUp(self):
+        print("\nRun setUp")
+
+    def tearDown(self):
+        print("Run tearDown")
+
+    @unittest.expectedFailure
     def test_sum_pos_int(self):
         self.assertEqual(sum(4, 6), 0)
 
     def test_sum_neg_int(self):
         self.assertEqual(sum(-4, -6), -10)
 
+    @unittest.skip('demonstrating skipping')
     def test_sum_pos_float(self):
         self.assertEqual(sum(4.5, 6.2), 10.7)
 
     def test_sum_neg_float(self):
         self.assertEqual(sum(-4.5, -6.2), -10.7)
         
+   """
 if __name__ == '__main__':
     unittest.main()
